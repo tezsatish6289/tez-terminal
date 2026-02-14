324b85f73ae9b61326b2fe6aa7021960062b7be6
Satish Sharma <hello@tezterminal.com>
2026-02-14 06:22:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6235588950-a15f2",
+  "appId": "1:632795229776:web:5a505b720a576c75caab55",
+  "apiKey": "AIzaSyD-6s2cwb9rz2Hgrvkd_AhAfrGbxaaacbc",
+  "authDomain": "studio-6235588950-a15f2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "632795229776"
+};
